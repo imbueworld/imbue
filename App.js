24e9b3e9d56709e1145a532dd80b1f5aa811de6d
kd0010 <kanders.davids@gmail.com>
2020-07-22 06:08:54 +0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import Home from "./assets/js/screens/Home"
+import Test from "./assets/js/screens/Test"
+import SignUp from "./assets/js/screens/SignUp"
+import Login from "./assets/js/screens/Login"
+
+const Stack = createStackNavigator()
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Home"
+                    component={Home}
+                    options={{ title: "Welcome" }}
+                />
+                <Stack.Screen
+                    name="Test"
+                    component={Test}
+                    options={{ title: "Test Screen" }}
+                />
+                <Stack.Screen
+                    name="SignUp"
+                    component={SignUp}
+                    options={{ title: "Sign Up" }}
+                />
+                <Stack.Screen
+                    name="Login"
+                    component={Login}
+                    options={{ title: "Login" }}
+                />
+                {/*
+                <Stack.Screen
+                    name=""
+                    component={}
+                    options={{ title: "" }}
+                />
+                */}
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
