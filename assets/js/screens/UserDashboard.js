9aa0b859b667b3b56f6b297cc7ddc1d4184ef88b
themanifestgarden <cormacncheese@gmail.com>
2020-10-18 15:18:00 -0600

    implementated custom map styling for iOS

--- assets/js/screens/UserDashboard.js
+++ assets/js/screens/UserDashboard.js
@@ -4,7 +4,6 @@ import { StyleSheet, View, Animated, TouchableHighlight, BackHandler } from 'rea
 import { useDimensions } from '@react-native-community/hooks'
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'
 
-import { mapStyle, mapStyle2 } from "../contexts/MapStyle"
 import ProfileLayout from "../layouts/ProfileLayout"
 
 import CustomButton from "../components/CustomButton"
@@ -175,6 +174,166 @@ export default function UserDashboard(props) {
     else sidePanelSlideIn()
   }, [expanded])
 
+  var mapStyle = [
+    {
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#f5f5f5"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#616161"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#f5f5f5"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.land_parcel",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#bdbdbd"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#eeeeee"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#e5e5e5"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#ffffff"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#dadada"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#616161"
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+      ]
+    },
+    {
+      "featureType": "transit.line",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#e5e5e5"
+        }
+      ]
+    },
+    {
+      "featureType": "transit.station",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#eeeeee"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#c9c9c9"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+      ]
+    }
+  ]
 
 
   return (
@@ -182,16 +341,15 @@ export default function UserDashboard(props) {
       <AlgoliaSearchAbsoluteOverlay style={{position: 'absolute', top: 300, }}/>
 
     <MapView
+      provider={PROVIDER_GOOGLE}
       style={styles.map}
-      // provider={PROVIDER_GOOGLE}
       initialRegion={{
         latitude: 37.78825, 
         longitude: -122.4324,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       }}
-      // customMapStyle={mapStyle2}
-
+      customMapStyle={mapStyle}
     >
       { Markers }
     </MapView>
@@ -337,9 +495,14 @@ const styles = StyleSheet.create({
     zIndex: 100,
   },
   map: {
-    width: "100%",
-    height: "100%",
-    backgroundColor: "#addbff", // water fill before map loads
+    position:'absolute',
+    top:0,
+    left:0,
+    right:0,
+    bottom:0,
+    // width: "100%",
+    // height: "100%",
+    // backgroundColor: "#addbff", // water fill before map loads
     zIndex: -1000,
   },
   badgeContainer: {
