b9592300be2f3a50b1c8030b58bcde5742c01d44
themanifestgarden <cormacncheese@gmail.com>
2020-12-21 15:03:57 -0800

    for cormac

---
+++ functions/src/SendGrid.js
@@ -0,0 +1,6 @@
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
+const { p } = require('./HelperFunctions')
+
+
+const apiKey = process.env.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;
+
