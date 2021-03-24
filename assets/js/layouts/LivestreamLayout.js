6bafc2696857f2a7397816dcfa79568597c7d036
SashaVons <vonswork@gmail.com>
2021-03-24 19:25:35 +0100

    Fix bug

--- assets/js/layouts/LivestreamLayout.js
+++ assets/js/layouts/LivestreamLayout.js
@@ -342,7 +342,7 @@ export default function LivestreamLayout(props) {
           zIndex: 105,
           marginTop: 40,
         }}>
-        {isInternetReachable && (
+        {!isInternetReachable && (
           <View
             style={{
               position: 'absolute',
